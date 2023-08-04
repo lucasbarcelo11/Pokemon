e6e3e543e510757b114f25ebf8cc604ba2e1aa95
lucasb <lucasbarcelo1981@gmail.com>
2023-08-04 17:31:37 -0300

    terminando2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,36 +1,37 @@
-import { useRef } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { setTrainerG } from "../store/slices/trainer.slice"
-import { useNavigate } from "react-router-dom"
-
+import { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setTrainerG } from "../store/slices/trainer.slice";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const inputTrainer = useRef();
 
-    const inputTrainer = useRef()
+  const dispatch = useDispatch();
 
-    const dispatch =  useDispatch()
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //e.target.inputTrainer.value
+    dispatch(setTrainerG(inputTrainer.current.value.trim()));
+    navigate("/pokedex");
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        //e.target.inputTrainer.value
-        dispatch(setTrainerG(inputTrainer.current.value.trim()))
-        navigate('/pokedex')
-        
-    }
-
-
-    return(
-        <div>
-            <h1>Pokedex</h1>
-            <h2>Hi Trainer</h2>
-            <p>To start with the app, give me your name trainer 😁</p>
-            <form onSubmit={handleSubmit}>
-                <input id="inputTrainer" ref={inputTrainer} type="text" />
-                <button>Gotta catch'em all!</button>
-            </form>
-        </div>
-    )
-}
-export default HomePage
+  return (
+    <div>
+      <img src="/pdx.png" alt="" width={800} className="mx-auto mt-5 pt-5" />
+      <h2 className="text-center mt-5">Hi Trainer</h2>
+      <p className="text-center">
+        To start with the app, give me your name trainer 😁
+      </p>
+      <div className="d-flex justify-content-center mx-auto align-items-end" style={{width:400, height: 150}}>
+        <form className="d-flex flex-row" onSubmit={handleSubmit}>
+          <input className="" id="inputTrainer" ref={inputTrainer} type="text" />
+          <br />
+          <button className="btn btn-danger ms-5">Gotta catch'em all!</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default HomePage;
--- src/pages/PokedexPage.jsx
+++ src/pages/PokedexPage.jsx
@@ -1,39 +1,35 @@
-import { useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import useFetch from "../hooks/useFetch";
 import { useEffect, useRef, useState } from "react";
 import PokeCard from "../components/pokedexPage/PokeCard";
 import SelectType from "../components/pokedexPage/SelectType";
 
 const PokedexPage = () => {
-
-  const [inputValue, setInputValue] = useState('')
-  const [selectValue, setSelectValue] = useState('allPokemons')
-  console.log(selectValue)
+  const [inputValue, setInputValue] = useState("");
+  const [selectValue, setSelectValue] = useState("allPokemons");
+  console.log(selectValue);
 
   const trainer = useSelector((reducer) => reducer.trainer);
 
-  const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=600'
-  const [ pokemons, getAllPokemons, getPokemonsByType ] = useFetch(url)
+  const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=600";
+  const [pokemons, getAllPokemons, getPokemonsByType] = useFetch(url);
 
   useEffect(() => {
-    if(selectValue === 'allPokemons' ) {
-      getAllPokemons()
+    if (selectValue === "allPokemons") {
+      getAllPokemons();
     } else {
-      getPokemonsByType(selectValue)
+      getPokemonsByType(selectValue);
     }
-   
-  }, [selectValue])
+  }, [selectValue]);
 
-  const inputSearch = useRef()
+  const inputSearch = useRef();
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    setInputValue(inputSearch.current.value.trim().toLowerCase())
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInputValue(inputSearch.current.value.trim().toLowerCase());
+  };
 
-
-  const cbFilter = poke => poke.name.includes(inputValue)
-
+  const cbFilter = (poke) => poke.name.includes(inputValue);
 
   return (
     <div>
@@ -41,19 +37,20 @@ const PokedexPage = () => {
         <span>Welcome {trainer}</span>, here you can find your favorite pokemon
       </p>
       <form onSubmit={handleSubmit}>
-        <input ref={inputSearch} onChange={(e) => setInputValue(e.target.value)} type="text" />
+        <input
+          ref={inputSearch}
+          onChange={(e) => setInputValue(e.target.value)}
+          type="text"
+        />
         <button>Search</button>
       </form>
-      <SelectType setSelectValue={setSelectValue}/>
-      <div>
-        {
-          pokemons?.results.filter(cbFilter).map(poke => (
-            <PokeCard
-              key={poke.url}
-              url={poke.url}
-            />
-          ))
-        }
+      <SelectType setSelectValue={setSelectValue} />
+      <div className="d-flex justify-content-center">
+        <div className="grids">
+          {pokemons?.results.filter(cbFilter).map((poke) => (
+            <PokeCard key={poke.url} url={poke.url} />
+          ))}
+        </div>
       </div>
     </div>
   );
